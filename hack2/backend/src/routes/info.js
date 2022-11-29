0af0e21807da956ffa783de1c17e95aca5ea2c25
Fuchuan Chung <foodchain@FuchuandeMacBook-Pro-5838.local>
2022-11-29 12:25:47 +0800

    hack2

---
+++ hack2/backend/src/routes/info.js
@@ -0,0 +1,75 @@
+// * ////////////////////////////////////////////////////////////////////////
+// *
+// * FileName     [ info.js ]
+// * PackageName  [ server ]
+// * Synopsis     [ Get restaurant info from database ]
+// * Author       [ Chin-Yi Cheng ]
+// * Copyright    [ 2022 11 ]
+// *
+// * ////////////////////////////////////////////////////////////////////////
+
+import InfoModel from '../models/info'
+import Info from '../models/info'
+
+exports.GetSearch = async (req, res) => {
+    /*******    NOTE: DO NOT MODIFY   *******/
+    const priceFilter = req.query.priceFilter
+    const mealFilter  = req.query.mealFilter
+    const typeFilter  = req.query.typeFilter
+    const sortBy      = req.query.sortBy
+    /****************************************/
+
+    // NOTE Hint: 
+    // use `db.collection.find({condition}).exec(err, data) {...}`
+    // When success, 
+    //   do `res.status(200).send({ message: 'success', contents: ... })`
+    // When fail,
+    //   do `res.status(403).send({ message: 'error', contents: ... })` 
+    let search = []
+    // console.log(mealFilter, typeFilter)
+    if (mealFilter.length !== 0 && typeFilter.length !== 0){
+        search = mealFilter.mealFilter.concat(typeFilter.typeFilter)
+    }
+    // console.log(search)
+    // TODO Part I-3-a: find the information to all restaurants
+    Info.find({price: priceFilter.priceFilter, tag: {$all: search }}).exec((err, data)=>{
+        if (err) {
+            // console.log('DB Get Error' + err)
+            res.status(403).send({
+                message: 'error',
+                content: err
+            })
+        }
+        else {
+            // console.log(data);
+            res.status(200).send({
+                message: 'success',
+                content: data
+            })
+            
+        }
+    });
+    
+    // TODO Part II-2-a: revise the route so that the result is filtered with priceFilter, mealFilter and typeFilter
+    // TODO Part II-2-b: revise the route so that the result is sorted by sortBy
+}
+
+exports.GetInfo = async (req, res) => {
+    /*******    NOTE: DO NOT MODIFY   *******/
+    const id = req.query.id
+    /****************************************/
+
+    // NOTE USE THE FOLLOWING FORMAT. Send type should be 
+    // if success:
+    // {
+    //    message: 'success'
+    //    contents: the data to be sent. Hint: A dictionary of the restaruant's information.
+    // }
+    // else:
+    // {
+    //    message: 'error'
+    //    contents: []
+    // }
+
+    // TODO Part III-2: find the information to the restaurant with the id that the user requests
+}
